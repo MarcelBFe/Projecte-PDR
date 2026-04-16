bfbb10edff5b773868973349d45eea47a05ffb41
Drato1007 <tonimch10@gmail.com>
2026-04-16 11:47:12 +0200

    Update tests.c

--- Organitzador tasques/Organitzador tasques/tests.c
+++ Organitzador tasques/Organitzador tasques/tests.c
@@ -1,4 +1,25 @@
-void tests_tasca_1(){
+#include <stdio.h>
+#include "tests.h"
+#include "tasca.h"
+
+
+void tests_tasca1() {
+    printf("--- INICI TEST TASCA 1 ---\n");
+
+    t_tasca mi_tarea;
+    mi_tarea = llegeix_nova_tasca();
     
-  
+    printf("\nComprovant mostra_tasca:\n");
+    mostra_tasca(mi_tarea);
+
+    printf("\nAra provarem els comentaris:\n");
+    t_comentari mi_comentario;
+    mi_comentario = llegeix_nou_comentari();
+    
+    printf("\nComprovant mostra_comentari:\n");
+    mostra_comentari(mi_comentario);
+
+    printf("\n--- FI TEST TASCA 1 ---\n");
+}
+    
 }
