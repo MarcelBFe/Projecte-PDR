279e9a585cbbb04315aec658a3bbdffe4b1bfede
Drato1007 <tonimch10@gmail.com>
2026-04-19 17:52:31 +0200

    Update tests.c

--- Organitzador tasques/Organitzador tasques/tests.c
+++ Organitzador tasques/Organitzador tasques/tests.c
@@ -23,3 +23,29 @@ void tests_tasca1() {
 }
     
 }
+
+#include <stdio.h>
+#include "tests.h"
+#include "carpeta.h"
+
+void tests_carpeta1() {
+    printf("\n--- INICI TEST CARPETA 1 ---\n");
+
+    t_carpeta mi_carpeta = llegeix_nova_carpeta();
+
+    printf("Introduce la fecha 1 (ej: 31/12/2026):\n");
+    t_data d1 = llegeix_nova_data();
+    printf("Introduce la fecha 2 (ej: 01/01/2026):\n");
+    t_data d2 = llegeix_nova_data();
+    printf("Introduce la fecha 3 (ej: 15/06/2026):\n");
+    t_data d3 = llegeix_nova_data();
+
+    insereix_nova_data(&mi_carpeta, d1);
+    insereix_nova_data(&mi_carpeta, d2);
+    insereix_nova_data(&mi_carpeta, d3);
+
+    printf("\nResultat del test (haurien de sortir ordenades):\n");
+    mostra_carpeta(mi_carpeta);
+
+    printf("\n--- FI TEST CARPETA 1 ---\n");
+}
