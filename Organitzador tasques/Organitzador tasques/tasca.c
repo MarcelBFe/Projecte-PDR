2016d751a1b1b1d0219ee198ff31630fd9796642
MarcelBF <153659157+MarcelBFe@users.noreply.github.com>
2026-04-24 13:38:25 +0200

    comentaris

--- Organitzador tasques/Organitzador tasques/tasca.c
+++ Organitzador tasques/Organitzador tasques/tasca.c
@@ -55,28 +55,28 @@ void mostra_tasca(t_tasca tas){
     } 
 }
 
-//Inserta la tascas en la posicio 0 del array, si s'ha pogut retorna 0 si no -1
+/*Inserta la tascas en la posicio 0 del array, si s'ha pogut retorna 0 si no -1 */
 int insereix_comentari(t_tasca *tas, t_comentari com){
     
-    //Definim variables
+    /*Definim variables */
     int i;
 
-//Comprova que hi hagi suficient espai per al nou comentari
+/*Comprova que hi hagi suficient espai per al nou comentari */
     if(tas->ncomentaris < MAX_COM){
 
-        //Fem un espai per a introduir el nou comentari
+        /*Fem un espai per a introduir el nou comentari */
         for(i=tas->ncomentaris;i>0;i--){
             tas->comentaris[i] = tas->comentaris[i-1];
         }
 
-        //Introduim el nou comentai e incrementem el contador
+        /*Introduim el nou comentai e incrementem el contador */
         tas->comentaris[0] = com;
         tas->ncomentaris++;
 
         return 0;
 
   }else{
-    //En cas de no tenir suficient espai per a incerir el comentari retonem -1
+    /*En cas de no tenir suficient espai per a incerir el comentari retonem -1 */
     return -1;
   }
   
@@ -84,24 +84,24 @@ int insereix_comentari(t_tasca *tas, t_comentari com){
 
 int elimina_comentari(t_tasca *tas, int num){
 
-    //Definim variables
+    /*Definim variables */
     int i;
 
-    //Comprovem que el numero del usuari existeix en el array de comentaris
+    /*Comprovem que el numero del usuari existeix en el array de comentaris */
     if(num > 0 && num <= tas->ncomentaris){
 
-        //Amb un bucle reposicionem els comentaris fins "aixafar el que volem eliminar"
+        /*Amb un bucle reposicionem els comentaris fins "aixafar el que volem eliminar" */
         for(i=num-1;i<tas->ncomentaris-1;i++){
             tas->comentaris[i] = tas->comentaris[i+1];
         }
 
-        //Reduim el numero de comentaris
+        /*Reduim el numero de comentaris */
         tas->ncomentaris--;
 
         return 0;
 
     }else{
-        //En cas de un nombre invalid retornem un -1
+        /*En cas de un nombre invalid retornem un -1 */
         return -1;
     }
 }
